d1e6667cf121d1b0e81f46494273e586c4546a03
Guilherme <anglim.guilerme@gmail.com>
2025-06-22 16:12:05 -0300

    arrumando js para os personagens

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,18 @@ window.addEventListener('scroll', function () {
 
     lastScroll = currentScroll;
 });
+
+const hamburguer = document.querySelector('.hamburguer');
+const headerMenu = document.querySelector('.menu-header');
+
+function toggleMenu(){
+    hamburguer.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+}
+
+hamburguer.addEventListener('click', toggleMenu);
+headerMenu.addEventListener('click', (event) => {
+    if (event.target.classList.contains('item-menu')) {
+        toggleMenu();
+    }
+});
