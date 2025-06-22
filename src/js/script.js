da3f6f7f28a217065e4a0c9e0922c9f91fd8336b
Guilherme <anglim.guilerme@gmail.com>
2025-06-22 16:16:36 -0300

    criando um pouco de responsividade

--- src/js/script.js
+++ src/js/script.js
@@ -15,17 +15,3 @@ window.addEventListener('scroll', function () {
     lastScroll = currentScroll;
 });
 
-const hamburguer = document.querySelector('.hamburguer');
-const headerMenu = document.querySelector('.menu-header');
-
-function toggleMenu(){
-    hamburguer.classList.toggle('active');
-    headerMenu.classList.toggle('active');
-}
-
-hamburguer.addEventListener('click', toggleMenu);
-headerMenu.addEventListener('click', (event) => {
-    if (event.target.classList.contains('item-menu')) {
-        toggleMenu();
-    }
-});
